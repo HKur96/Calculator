0b9beabc4ef581223611dbf98320ac94ffa49788
John Kramer <hkur.gobimbel@gmail.com>
2023-10-28 21:46:45 +0700

    Initial Commit

---
+++ Calculator App/Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_AppApp.swift
+//  Calculator App
+//
+//  Created by Mac on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
