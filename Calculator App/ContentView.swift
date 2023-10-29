5fadebc15b1b90cde84a3494eeefd3b386dec5b4
John Kramer <hkur.gobimbel@gmail.com>
2023-10-29 14:51:57 +0700

    first commit

--- Calculator App/ContentView.swift
+++ Calculator App/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var firstNumber: String = ""
+    @State private var secondNumber: String = ""
+    @State private var operation: String = ""
+    @State private var result: String = ""
+    var calc = Calculator()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20){
+            CustomTextField(title: "First Number", text: $firstNumber)
+            
+            Picker("Picker", selection: $operation) {
+                Text("+").tag("+")
+                Text("-").tag("-")
+                Text("*").tag("*")
+                Text("/").tag("/")
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            
+            CustomTextField(title: "Second Number", text: $secondNumber)
+            
+            Text(result)
+                .font(.title)
+                .foregroundColor(.mint)
+            
+            Button {
+                result = calc.calculate(firstNumber: firstNumber, secondNumber: secondNumber, operation: operation)
+            } label: {
+                Text("calculate")
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(10)
         }
-        .padding()
+    }
+}
+
+class Calculator {
+    func calculate(firstNumber: String, secondNumber: String, operation: String) -> String {
+        let num1 = Int(firstNumber) ?? 0
+        let num2 = Int(secondNumber) ?? 0
+        
+        switch operation {
+        case "+" :
+            return String(num1 + num2)
+        case "-":
+            return String(num1 - num2)
+        case "*":
+            return String(num1 * num2)
+        default:
+            return String(num1 / num2)
+        }
     }
 }
 
